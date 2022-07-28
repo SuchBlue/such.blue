f4a2661ad461d80ff6ac03e74c30d8f57fbf6a22
SuchBlue <vin.36386@gmail.com>
2022-07-28 14:42:05 +0200

    Implemented new backend for server stats

--- stats/data.js
+++ stats/data.js
@@ -1,23 +1,11 @@
 function load() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
-        const responseJSON = JSON.parse(this.responseText
-                .replace('"system.cpu"', '"systemcpu"')
-                .replace('"system.ram"', '"systemram"'));
-            
-        const cpuUsage = responseJSON.systemcpu.dimensions.user.value + responseJSON.systemcpu.dimensions.system.value;
-
-        const totalRam = responseJSON.systemram.dimensions.used.value
-            + responseJSON.systemram.dimensions.free.value
-            + responseJSON.systemram.dimensions.cached.value
-            + responseJSON.systemram.dimensions.buffers.value;
-            
-        const ramUsage = responseJSON.systemram.dimensions.used.value;
-        document.getElementById("cpuStat").innerText = `${Math.round(cpuUsage * 100) / 100}% (3.6GHz)`;
-        // ^^ I don't know how I'll be able to get the CPU frequency, so I've just hardcoded it so far ~SuchBlue
-        document.getElementById("memStat").innerText = `${(ramUsage / 1000).toFixed(2)}/${(totalRam / 1000).toFixed(1)}GB ${Math.round((ramUsage / totalRam) * 10000) / 100}%`;
+        const responseJSON = JSON.parse(this.responseText);
+        document.getElementById("cpuStat").innerText = `${responseJSON.cpu}% (${responseJSON.cpuFreq}GHz)`;
+        document.getElementById("memStat").innerText = `${responseJSON.usedmem}/${responseJSON.totalmem}GB ${responseJSON.ram}%`;
     }
-    xhttp.open("GET", "https://home.such.blue/netdata/api/v1/allmetrics?format=json");
+    xhttp.open("GET", "https://home.such.blue/stats-api");
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState !== 4) {
             return;
@@ -33,4 +21,4 @@ function load() {
     xhttp.send();
 }
 load();
-setInterval(load, 10000);
+setInterval(load, 8000);
