956f35f8ea8960d1a71b96ca7265b2aa11c60bd1
SuchBlue <58862188+SuchBlue@users.noreply.github.com>
2023-01-14 20:40:34 +0100

    Removed unnecessary old code

--- _backend/index.js
+++ _backend/index.js
@@ -3,31 +3,6 @@ const fs = require('fs');
 const app = express();
 const port = 5000;
 
-const os = require('os');
-const osu = require('node-os-utils');
-
-let cpuUsage;
-
-function getCPU() {
-	osu.cpu.usage()
-	.then(cpuPercentage => {
-		cpuUsage = cpuPercentage;
-	});
-}
-
-app.get('//stats', (req, res) => {
-	let usedmem = os.totalmem() - os.freemem();
-
-	let summary = {
-		cpu: cpuUsage,
-		ram: (Math.round((usedmem / os.totalmem()) * 10000) / 100),
-		cpuFreq: (os.cpus()[0].speed / 1000).toFixed(2),
-		usedmem: (usedmem / 1000000000).toFixed(2),
-		totalmem: (os.totalmem() / 1000000000).toFixed(1),
-	}
-	res.send(JSON.stringify(summary));
-});
-
 app.get('//getViews', (req, res) => {
 	fs.readFile('views', 'utf-8', function(err, data) {
 		if(err) console.log(err);
@@ -53,6 +28,4 @@ app.listen(port, () => {
 		if(err) console.log(err);
 	})
   }
-
-  getCPU(); setInterval(getCPU, 5000);
 });
