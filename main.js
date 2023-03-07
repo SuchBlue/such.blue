eb58b011fc41ec6cc7819f8941151c53e28672a0
SuchBlue <58862188+SuchBlue@users.noreply.github.com>
2023-03-07 20:44:52 +0100

    Made server status more integrated

--- main.js
+++ main.js
@@ -18,16 +18,23 @@ window.onload = function() {
     .catch(error => console.error(error));
 
     let xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-        document.getElementById("views").innerText = `This site has been visited for ${this.responseText} times`;
-    }
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
+                document.getElementById("views").innerText = `${this.responseText}`;
+            } else {
+                document.getElementById("viewsParent").innerText = "Server is offline.";
+            }
+        }
+    };
+
     xhttp.open("GET", "https://cloud.such.blue/api/getViews");
     xhttp.send();
 
     let time = new Date().getTime() - window.performance.timing.navigationStart;
     let seconds = Math.floor(time / 1000);
     let milliseconds = time % 1000;
-    document.getElementById("time").innerText = `Loaded in ${seconds}.${milliseconds} seconds`;
+    document.getElementById("time").innerText = `${seconds}.${milliseconds}`;
     document.getElementById("time").style.marginBottom = `${window.innerHeight/4}px`;
 
     addView();
