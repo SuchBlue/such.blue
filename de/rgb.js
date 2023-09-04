83b77daeb378d29baaec6ebcb0341825840d3278
SuchBlue <58862188+SuchBlue@users.noreply.github.com>
2023-09-04 19:44:52 +0200

    Added German translation @ suchblue.de

---
+++ de/rgb.js
@@ -0,0 +1,28 @@
+let btn = document.getElementById("rgb");
+let rgbCSS = `
+animation: colorchange 20s infinite alternate;
+-webkit-animation: colorchange 20s infinite alternate;
+`
+
+function addRGB() {
+    document.body.style += rgbCSS;
+    document.getElementById("footer").style += rgbCSS;
+}
+function removeRGB() {
+    document.body.style -= rgbCSS;
+    document.getElementById("footer").style -= rgbCSS;
+}
+
+addRGB();
+
+btn.addEventListener('click', function handleClick() {
+    const initialText = 'an';
+  
+    if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+      removeRGB();
+      btn.textContent = 'aus';
+    } else {
+      addRGB();
+      btn.textContent = initialText;
+    }
+  });
