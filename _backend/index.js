a810afdee10a9893a3499757c7522c75ba4cd294
SuchBlue <58862188+SuchBlue@users.noreply.github.com>
2024-06-18 16:52:02 +0200

    Ratelimiter

--- _backend/index.js
+++ _backend/index.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const app = express();
 const port = 5000;
 
-app.get('//getViews', (req, res) => {
+app.get('//getViews', /*limiter,*/ (req, res) => {
 	fs.readFile('views', 'utf-8', function(err, data) {
 		if(err) console.log(err);
 		res.send(data);
 	});
 });
 
-app.post('//setViews', (req, res) => {
+app.post('//setViews', limiter, (req, res) => {
 	fs.readFile('views', 'utf-8', function(err, data) {
 		if(err) console.log(err);
 
