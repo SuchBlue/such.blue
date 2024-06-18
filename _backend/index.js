5bd686bf54565829dfb425bf9800ecfd393f03a8
SuchBlue <58862188+SuchBlue@users.noreply.github.com>
2024-06-18 17:16:32 +0200

    backend fixes

--- _backend/index.js
+++ _backend/index.js
@@ -1,8 +1,26 @@
-import { limiter } from "./ratelimiter";
+const { RateLimiterMemory } = require('rate-limiter-flexible');
 const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 5000;
+
+const rateLimiter = new RateLimiterMemory({
+    points: 3,
+    duration: 120
+});
+
+const limiter = async(req, res, next) => {
+    try {
+        await rateLimiter.consume(req.connection.remoteAddress);
+        next();
+    } catch(error) {
+        res.set({
+            "Retry-After": error.msBeforeNext / 1000,
+            "X-RateLimit-Reset": new Date(Date.now() + error.msBeforeNext)
+        });
+        return res.status(429).send({error: "Too many requests."})
+    }
+}
 
 app.get('//getViews', /*limiter,*/ (req, res) => {
 	fs.readFile('views', 'utf-8', function(err, data) {
